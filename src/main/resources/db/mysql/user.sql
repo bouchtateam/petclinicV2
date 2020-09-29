c7e45cc8b7ea6aa92a9d3a26dac4019bf13216fc
bouchtateam <34681030+bouchtateam@users.noreply.github.com>
2020-09-29 18:12:20 +0100

    Update user.sql

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
+CREATE DATABASE IF NOT EXISTS petclinic1;
 
-ALTER DATABASE petclinic
+ALTER DATABASE petclinic1
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic@%' IDENTIFIED BY 'petclinic';
+GRANT ALL PRIVILEGES ON petclinic1.* TO 'petclinic@%' IDENTIFIED BY 'petclinic';
